06c4ead3da39cc7b59606b1758eca69350900f13
Son Nguyen <business@banhmi.dev>
2025-06-07 10:14:55 +0200

    Update colorscheme to transparent NVIM

--- .config/nvim/lua/banhmidev/plugins/colorscheme.lua
+++ .config/nvim/lua/banhmidev/plugins/colorscheme.lua
@@ -2,11 +2,9 @@ return {
 	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-		local transparent = false
-
 		require("tokyonight").setup({
 			style = "night",
-			transparent = transparent,
+			transparent = true,
 			styles = {
 				sidebars = transparent and "transparent" or "dark",
 				floats = transparent and "transparent" or "dark",
